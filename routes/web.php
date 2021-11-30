43abe90878e884c602efa39916d7c88930a5a260
JJNering <89945493+JJNering@users.noreply.github.com>
2021-11-30 10:06:15 -0400

    Melhorias ao relacionamento de perfis e permissoes

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,11 @@ Route::prefix('admin')
     /**
      * Route Permissions
      */
+    Route::get('profiles/{id}/permission/{idPermission}/detach', 'ACL\PermissionProfileController@detachPermissionProfile')->name('profiles.permission.detach');
     Route::post('profiles/{id}/permissions', 'ACL\PermissionProfileController@attachPermissionsProfile')->name('profiles.permissions.attach');
-    Route::get('profiles/{id}/permissions/create', 'ACL\PermissionProfileController@permissionsAvailable')->name('profiles.permissions.available');
+    Route::any('profiles/{id}/permissions/create', 'ACL\PermissionProfileController@permissionsAvailable')->name('profiles.permissions.available');
     Route::get('profiles/{id}/permissions', 'ACL\PermissionProfileController@permissions')->name('profiles.permissions');
+    Route::get('permissions/{id}/profile', 'ACL\PermissionProfileController@profiles')->name('permissions.profiles');
 
     /**
      * Route Permissions
