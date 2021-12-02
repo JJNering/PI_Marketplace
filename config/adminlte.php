1b6a7dc08f19fb7df9135a5f9ca31fdff59ff19e
Igor-Gustavo-B-BESERRA <95254035+Igor-Gustavo-B-BESERRA@users.noreply.github.com>
2021-12-02 18:04:11 -0400

    Inclusão melhorias para produtos

--- config/adminlte.php
+++ config/adminlte.php
@@ -265,7 +265,7 @@ return [
         [
             'text' => 'Produtos',
             'url'  => 'admin/products',
-            'icon' => 'fas fa-layer-group'
+            'icon' => 'fas fa-utensils'
         ],
 
 
